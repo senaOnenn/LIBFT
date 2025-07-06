0c2c7d742443bfbca1e208e48718ede8f9f7ecd5
Emine Sena" Önen <eonen@k1m37s01.42istanbul.com.tr>
2025-07-06 11:24:18 +0300

    dosyalar yüklendi

---
+++ ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eonen <eonen@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/20 17:06:22 by eonen             #+#    #+#             */
+/*   Updated: 2025/07/01 17:29:28 by eonen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+#include <stddef.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*ptr;
+	size_t				i;
+
+	ptr = (const unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (ptr[i] == (unsigned char)c)
+			return ((void *)&ptr[i]);
+		i++;
+	}
+	return (NULL);
+}
---
+++ ft_substr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eonen <eonen@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/26 14:18:59 by eonen             #+#    #+#             */
+/*   Updated: 2025/07/01 17:43:54 by eonen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	slen;
+	size_t	actuallen;
+
+	if (s == NULL)
+		return (NULL);
+	slen = ft_strlen(s);
+	if (start >= slen)
+		actuallen = 0;
+	else
+	{
+		if (len + start > slen)
+			actuallen = slen - start;
+		else
+			actuallen = len;
+	}
+	substr = (char *)malloc(actuallen + 1);
+	if (substr == NULL)
+		return (NULL);
+	ft_memcpy(substr, s + start, actuallen);
+	substr[actuallen] = '\0';
+	return (substr);
+}
